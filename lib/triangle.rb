5f1edaca1150cc207179bef0c21a486a1e09bccf
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-13 19:10:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 class Triangle
-  attr_accessor :all
 
   def initialize(side1, side2, side3)
     @side1 = side1
@@ -9,6 +8,24 @@ class Triangle
     @side3 = side3
     @all = [@side1, @side2, @side3]
   end
+
+  def kind
+    if @all.any? { |side| !(side.positive?) } || @side1 + @side2 < @side3 || @side1 + @side3 < @side2 || @side2 + @side3 < @side1
+      raise TriangleError
+    else
+      if @all.uniq.length == 1
+        :equilateral
+      elsif @all.uniq.length == 2
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+  
 end
 
 binding.pry
