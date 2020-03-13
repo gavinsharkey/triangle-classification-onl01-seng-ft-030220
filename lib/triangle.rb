0d6bf90c58afbcf0dc6e3a9ab43f0e426f179ccb
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-13 18:58:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,14 @@
-class Triangle
-  attr_accessor :side1, :side2, :side3
+require 'pry'
 
+class Triangle
+  attr_accessor :all
+  
   def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @all = [@side1, @side2, @side3]
+  end
 end
+
+binding.pry
